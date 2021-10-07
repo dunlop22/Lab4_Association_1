2b8bd24b41590b03edc6bc2d8073c2e41c989374
Egor Golikov <20020110ll@gmail.com>
2021-10-07 10:15:47 +0700

    Добавьте файлы проекта.

---
+++ Lab4_Association_1/Avto.cpp
@@ -0,0 +1,80 @@
+#include "Avto.h";
+#include <iostream>
+using namespace std;
+
+Avto::Avto()
+{
+}
+
+
+void Avto::new_avto(Motor har2_1, Obchee har1_1, Korobka har4_1, Koleso har3_1)
+{
+    if (har2_1.motor_get() == 1 && har1_1.obchee_get() == 1 && har3_1.koleso_get() >= 0 && har4_1.korobka_get() == 1)
+    {
+        har1 = har1_1;
+        har2 = har2_1;
+        har3 = har3_1;
+        har4 = har4_1;
+    }
+    else
+    {
+        cout << "Сборка невозможна. Не вся информация об агрегатах добавлена";
+    }
+}
+
+void Avto::vod_avto(Voditel vod_1)
+{
+    if (Avto::har1.obchee_get() == 1 && vod_1.voditel_get() == 1)
+    {
+        vod = vod_1;
+        cout << "Водитель успешно привязан";
+    }
+    else
+    {
+        cout << "Информация об автомобиле или водителе не найдена";
+    }
+}
+
+void Avto::prosmotr_avto()
+{
+    if (Avto::har1.obchee_get() == 1)
+    {
+        Avto::har1.prosmotr_obchee();
+        Avto::har2.prosmotr_motor();
+        Avto::har3.prosmotr_koleso();
+        Avto::har4.prosmotr_korobka_peredach();
+        if (Avto::vod.voditel_get() == 1)
+        {
+            Avto::vod.prosmotr_voditel();
+        }
+    }
+    else
+    {
+        cout << "Информация об автомобиле не найдена";
+    }
+}
+
+void Avto::avto_otv()
+{
+    if (Avto::har1.obchee_get() == 1)
+    {
+        Avto::vod.Voditel::Voditel();
+        cout << "Водитель успешно отвязан от автомобиля";
+    }
+    else
+    {
+        cout << "Информация об автомобиле не найдена";
+    }
+}
+
+int Avto::avto_get()
+{
+    if (Avto::har2.motor_get() == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
---
+++ Lab4_Association_1/Avto.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "Motor.h";
+#include "Obchee.h";
+#include "Korobka.h";
+#include "Koleso.h";
+#include "Voditel.h";
+
+class Avto
+{
+	Obchee har1;
+	Motor har2;
+	Koleso har3;
+	Korobka har4;
+	Voditel vod;
+public:
+	Avto();
+	void new_avto(Motor har2_1, Obchee har1_1, Korobka har4_1, Koleso har3_1);
+	void vod_avto(Voditel vod_1);
+	void prosmotr_avto();
+	void avto_otv();
+	int avto_get();
+	~Avto() {};
+};
+
+
+
